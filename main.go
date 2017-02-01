8980f898ad8a671272d8552807b2e5da95ac9843
Niall Paterson <npaters@tcd.ie>
2017-02-01 11:23:31 +0000

    Initial Commit

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+)
+
+type Ontology struct {
+	XMLName                 xml.Name                `xml:"Ontology"`
+	AnnotationAssertions    []AnnotationAssertion   `xml:"AnnotationAssertion"`
+	SubAnnotationProperties []SubAnnotationProperty `xml:"SubAnnotationPropertyOf"`
+	Declarations            []Declaration           `xml:"Declaration"`
+	Subclasses              []Subclass              `xml:"SubClassOf"`
+}
+
+/* AnnotationAssertion */
+type AnnotationAssertion struct {
+	XMLName            xml.Name `xml:"AnnotationAssertion"`
+	AnnotationProperty AnnotationProperty
+	AbbreviatedIRI     AbbreviatedIRI
+	Literal            Literal
+}
+
+type AnnotationProperty struct {
+	XMLName        xml.Name       `xml:"AnnotationProperty"`
+	AbbreviatedIRI AbbreviatedIRI `xml:"abbreviatedIRI,attr"`
+}
+
+type AbbreviatedIRI struct {
+	XMLName xml.Name `xml:"AbbreviatedIRI"`
+	Value   string   `xml:",chardata"`
+}
+
+type Literal struct {
+	XMLName     xml.Name `xml:"Literal"`
+	DatatypeIRI string   `xml:"datatypeIRI,attr"`
+	Lang        string   `xml:"lang,attr"`
+	Value       string   `xml:",chardata"`
+}
+
+/* Sub Annotation Properties */
+
+type SubAnnotationProperty struct {
+	XMLName                  xml.Name `xml:"SubAnnotationPropertyOf"`
+	ParentAnnotationProperty AnnotationProperty
+	ChildAnnotationProperty  AnnotationProperty
+}
+
+/* Declaration */
+
+type Declaration struct {
+	XMLName xml.Name `xml:"Declaration"`
+	Class   Class
+}
+
+type Class struct {
+	XMLName        xml.Name       `xml:"Class"`
+	AbbreviatedIRI AbbreviatedIRI `xml:"abbreviatedIRI,attr"`
+}
+
+/* Subclass */
+
+type Subclass struct {
+	XMLName              xml.Name `xml:"SubclassOf"`
+	Class                Class
+	ObjectSomeValuesFrom ObjectSomeValuesFrom
+}
+
+type ObjectSomeValuesFrom struct {
+	XMLName        xml.Name `xml:"ObjectSomeValuesFrom"`
+	ObjectProperty ObjectProperty
+	Class          Class
+}
+
+type ObjectProperty struct {
+	XMLName        xml.Name       `xml:"ObjectProperty"`
+	AbbreviatedIRI AbbreviatedIRI `xml:"abbreviatedIRI,attr"`
+}
+
+func main() {
+	data, err := ioutil.ReadFile("./data/DINTO.owl")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	var ontology Ontology
+	xml.Unmarshal(data, &ontology)
+	fmt.Println(ontology)
+}
