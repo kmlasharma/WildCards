510aaf212f842a213bb328822e23ab5d76b8db56
GregBuckley <GregBuckley99@hotmail.com>
2017-02-01 10:09:53 -0800

    sudo needs to be used installing docker-compose

--- Scripts/install_docker_stack.sh
+++ Scripts/install_docker_stack.sh
@@ -5,7 +5,7 @@
 # Only install Docker if not already installed.
 # If Docker is already on the machine then redownloading it can cause issues
 which docker
-if [ $? -ne 1 ] 
+if [ $? -ne 1 ]
 	then
 		# Install dependencies
 		sudo apt-get install curl \
@@ -15,11 +15,11 @@ if [ $? -ne 1 ]
 		sudo apt-get update
 
 		# Install Docker
-		#curl -fsSL https://get.docker.com/ | sh
-		
+	#	curl -fsSL https://get.docker.com/ | sh
+
 		# Add Docker's GPG key
 		curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-		
+
 		# Add Docker as a known repository
 		sudo add-apt-repository \
 			"deb https://apt.dockerproject.org/repo/ \
@@ -30,9 +30,9 @@ if [ $? -ne 1 ]
 		# Install Docker Engine
 		sudo apt-get -y install docker-engine
 		sudo apt-get update
-		
+
 		# Install Docker Compose
-		curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-		chmod +x /usr/local/bin/docker-compose
+		sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+		sudo chmod +x /usr/local/bin/docker-compose
 fi
 
