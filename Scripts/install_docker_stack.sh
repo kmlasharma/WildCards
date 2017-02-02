26a16bd8d40c2bcc1e5963a138eb8982817a875c
Tomas Barry <tomas.barry@hotmail.com>
2017-02-02 16:16:34 +0000

    Changed install docker script to install the docker stack (docker, docker engine and docker compose

---
+++ Scripts/install_docker_stack.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Root permissions are required to run this script
+
+# Only install Docker if not already installed.
+# If Docker is already on the machine then redownloading it can cause issues
+which docker
+if [ $? -ne 1 ] 
+	then
+		# Install dependencies
+		sudo apt-get install curl \
+					software-properties-common \
+					apt-transport-https \
+					ca-certificates
+		sudo apt-get update
+
+		# Install Docker
+		#curl -fsSL https://get.docker.com/ | sh
+		
+		# Add Docker's GPG key
+		curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+		
+		# Add Docker as a known repository
+		sudo add-apt-repository \
+			"deb https://apt.dockerproject.org/repo/ \
+				ubuntu-$(lsb_release -cs) \
+				main"
+		sudo apt-get update
+
+		# Install Docker Engine
+		sudo apt-get -y install docker-engine
+		sudo apt-get update
+		
+		# Install Docker Compose
+		curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+		chmod +x /usr/local/bin/docker-compose
+fi
+
