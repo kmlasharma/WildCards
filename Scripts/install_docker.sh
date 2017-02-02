3e7da2e4594207ccf879b118a03137931a55a8f7
Niall Paterson <npaters@tcd.ie>
2017-02-02 15:47:21 +0000

    Update install_docker.sh

--- Scripts/install_docker.sh
+++ Scripts/install_docker.sh
@@ -5,12 +5,12 @@
 # Only install Docker if not already installed.
 # If Docker is already on the machine then redownloading it can cause issues
 which docker
-if [ $? -ne 0 ] 
-	then
-		# Install dependencies
-		sudo apt-get install -y curl
-		sudo apt-get update
-
+if [ $? -ne 0 ]; then
+		if [ "$(uname)" != "Darwin" ]; then
+			# This is a linux machine. Install dependencies
+			sudo apt-get install -y curl
+			sudo apt-get update
+		fi
 		# Install Docker
 		curl -fsSL https://get.docker.com/ | sh
 fi
