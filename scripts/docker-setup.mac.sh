65fcd012a9eb72d426ebe065e0798887743bdaa1
kmlasharma <sharmak@tcd.ie>
2017-02-12 14:14:13 +0000

    Update docker-setup.mac.sh

--- scripts/docker-setup.mac.sh
+++ scripts/docker-setup.mac.sh
@@ -5,7 +5,7 @@ docker-machine ls | tee >(wc -l)
 if [ $? -eq 1 ]; then
   docker-machine create --driver virtualbox default
   eval $(docker-machine env default)
-  docker-machine start default
 else
   printf "Docker machine already provisioned. You're good to go!\n"
 fi
+docker-machine start default
