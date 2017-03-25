9d495865cba50575d281eb1e1642609bda451a15
CraigArnott <arnottc@tcd.ie>
2017-03-25 12:31:29 +0000

    add resources for pml validation tests

--- pkgs/pml/pml_test.go
+++ pkgs/pml/pml_test.go
@@ -8,18 +8,19 @@ import (
 )
 
 func TestNoProcesses(t *testing.T) {
-	fmt.Println("* Testing loading PML file with no processes")
-	reader, _ := os.Open("/root/no_processes.pml") // empty file
-	parser := NewParser(reader)
 	// we use os.exit so it's not really possible to test this at the moment
-	process := parser.Parse()
-	if(assert.Nil(t, process, "Process should not exist")) {
-		fmt.Println("PASSED!")
-	}
+
+	//fmt.Println("* Testing loading PML file with no processes")
+	//reader, _ := os.Open("/root/no_processes.pml") // empty file
+	//parser := NewParser(reader)
+	//process := parser.Parse()
+	//if(assert.Nil(t, process, "Process should not exist")) {
+	//	fmt.Println("PASSED!")
+	//}
 }
 
 // our application doesn't currently handle multiple processes
-// we should probably add a method for fetching all processes
+// need clarification on whether or not this is required
 func TestMultipleProcesses(t * testing.T) {
 	//TODO
 	fmt.Println("* Testing loading PML file with multiple processes")
@@ -61,25 +62,31 @@ func TestMultipleTasksDrugs(t *testing.T) {
 }
 
 func TestValidateClashes(t * testing.T) {
-	// TODO
-	// Need clarification here: are we checking only for clashes in process names?
-	// What about a process that contains tasks with overlapping names?
-	// Or overlapping task names over different processes?
-	fmt.Println("* Testing that PML files with process name clashes are rejected")
-
-
-	fmt.Println("Unimplemented...")
+	// Need clarification here: are we checking only for clashes in sequence names?
+	// What about a sequence that contains actions with overlapping names?
+	// Or overlapping action names over different sequences?
+	// Or processes with overlapping names?
+	fmt.Println("* Testing that PML files with task name clashes are rejected")
+	reader, _ := os.Open("/root/sequence_clashes.pml") // empty file
+	parser := NewParser(reader)
+	process := parser.Parse()
+	err := process.Validate()
+	if(assert.Equal(t, err.Error(), "Multiply defined sequence: mySeq")) {
+		fmt.Println("PASSED!")
+	}
 }
 
 func TestValidateNoClashes(t * testing.T) {
-	// TODO
 	// Need clarification here: are we checking only for clashes in process names?
 	// What about a process that contains tasks with overlapping names?
 	// Or overlapping task names over different processes?
-	fmt.Println("* Testing that PML files with no process name clashes are not rejected")
-
-
-	fmt.Println("Unimplemented...")
+	fmt.Println("* Testing that PML files with no task name clashes are not rejected")
+	reader, _ := os.Open("/root/no_sequence_clashes.pml") // empty file
+	parser := NewParser(reader)
+	process := parser.Parse()
+	if(assert.Nil(t, process.Validate(), "There should be no task name clashes detected")) {
+		fmt.Println("PASSED!")
+	}
 }
 
 // TODO: tests for broken PML files
