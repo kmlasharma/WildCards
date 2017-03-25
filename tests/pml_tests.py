6009e7d61c611b8f48d2f84a6ed98dfd61eab427
Tomas <tomas.barry@hotmail.com>
2017-03-25 07:52:54 -0700

    Slight reordering to imports, uses environment variables

--- tests/pml_tests.py
+++ tests/pml_tests.py
@@ -1,10 +1,15 @@
 import test_utils
-import unittest
 
-class Test(unittest.TestCase):
+from os import environ
+from unittest import TestCase
+
+class Test(TestCase):
 
     # start app before each test case
     def setUp(self):
+        self.res_dir = environ['RES_DIR']
+        self.test_pml_file = self.res_dir + '/test.pml'
+
         self.app = test_utils.start_app()
 
     # kill app process after each test case
@@ -13,14 +18,13 @@ class Test(unittest.TestCase):
 
     # test for PML file selection & PML file loading
     def test_loading_pml(self):
-        self.app.input_pml_file('../test.pml')
-        self.app.wait_for_line('Enter path to OWL File: [default is test.owl] ')
+        self.app.input_pml_file(self.test_pml_file)
+        self.app.wait_for_line('Enter path to OWL File: [default is test.pml] ')
 
     # test for running PML analyis
     def test_pml_analysis(self):
-        self.app.input_pml_file('../test.pml')
+        self.app.input_pml_file(self.test_pml_file)
         res = self.app.get_process_drugs()
         #print(res)
         assert res == ['Plavix', 'Lipitor', 'Nexium']
-        self.app.input_owl_file('../test.owl')
         
