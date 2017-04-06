260896fa382b87b506c96c8615b4f4561c8bc2f6
Tomas <tomas.barry@hotmail.com>
2017-04-06 06:20:13 -0700

    Fixed failing test and added clearer indication that all tests pass

--- utils/scripts/test.sh
+++ utils/scripts/test.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+RESULT=0
 echo "Running test suite. This may take a minute.."
 for dir in `ls pkgs/`;
 do
     cd pkgs/$dir
     go test
+    RESULT=$?
     cd ../..
 done
+
+echo "####################################################################"
+echo "####################################################################"
+if [ $RESULT -eq 0 ]; then
+	echo "All tests passed"
+else
+	echo "Some tests failed"
+fi
+echo "####################################################################"
+echo "####################################################################"
+
