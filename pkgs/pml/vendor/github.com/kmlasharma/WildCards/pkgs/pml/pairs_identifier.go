0371462b3ea894a4be429131e2f102d02c4c7e4c
Niall Paterson <npaters@tcd.ie>
2017-04-09 12:53:34 +0100

    Bug Fixes

--- pkgs/pml/vendor/github.com/kmlasharma/WildCards/pkgs/pml/pairs_identifier.go
+++ pkgs/pml/vendor/github.com/kmlasharma/WildCards/pkgs/pml/pairs_identifier.go
@@ -278,10 +278,16 @@ func (ele *Element) parseIteration(params Params) Params {
 	iterationDelay := updatedParams.currentDelay - params.currentDelay
 	for _, pair := range updatedParams.drugPairs {
 		delay := iterationDelay - pair.Delay
+		var minDelay Delay
+		if delay < pair.Delay {
+			minDelay = delay
+		} else {
+			minDelay = pair.Delay
+		}
 		newPair := DrugPair{
 			DrugA:      pair.DrugB,
 			DrugB:      pair.DrugA,
-			Delay:      delay,
+			Delay:      minDelay,
 			ddiType:    pair.ddiType,
 			parentName: pair.parentName,
 		}
