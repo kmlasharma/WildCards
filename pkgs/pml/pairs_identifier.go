9b442ec1aafc9533fdfb58ef6cdee3510a446a2b
Tomas <tomas.barry@hotmail.com>
2017-04-09 04:25:22 -0700

    MIN DDI

--- pkgs/pml/pairs_identifier.go
+++ pkgs/pml/pairs_identifier.go
@@ -275,10 +275,16 @@ func (ele *Element) parseIteration(params Params) Params {
 	iterationDelay := updatedParams.currentDelay - params.currentDelay
 	for _, pair := range updatedParams.drugPairs {
 		delay := iterationDelay - pair.Delay
+		var minDelay int
+		if delay < pairDelay {
+			minDelay := delay
+		} else {
+			minDelay := pair.Delay
+		}
 		newPair := DrugPair{
 			DrugA:      pair.DrugB,
 			DrugB:      pair.DrugA,
-			Delay:      delay,
+			Delay:      minDelay,
 			ddiType:    pair.ddiType,
 			parentName: pair.parentName,
 		}
